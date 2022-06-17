603d7d51760043a6efe1174370abd75af1c6147b
Flavien Solt <flsolt@ethz.ch>
2022-06-17 14:37:34 +0200

    Dockerfiles, test scripts

---
+++ cellift-rocket/tests.sh
@@ -0,0 +1,19 @@
+set -e
+export SIMLEN=10000
+export TRACEFILE=$PWD/out.trace
+. ../../../cellift-meta/env.sh
+benchmarks=$CELLIFT_META_ROOT/benchmarks/out/rocket/bin/ # XXX wrong binary
+( cd $CELLIFT_META_ROOT/benchmarks && bash build-benchmarks.sh rocket )
+bin=$benchmarks/median.riscv
+# XXX probably need different RAM/ROM ELFS
+export SIMROMELF=$bin
+export SIMSRAMELF=$bin
+if [ ! -f $bin ]
+then
+    echo Benchmarks failed to build.
+    exit 1
+fi
+make run_vanilla_trace
+make run_vanilla_notrace
+make run_cellift_trace
+make run_cellift_notrace
